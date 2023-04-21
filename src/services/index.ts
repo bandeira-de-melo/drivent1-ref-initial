0cdeff2058f58b54c3735a211213c7e8d19a7ce7
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-04-21 20:08:48 -0300

    fix: export services index

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,5 +2,6 @@ export * from './users-service';
 export * from './authentication-service';
 export * from './events-service';
 export * from './enrollments-service';
-// eslint-disable-next-line import/export
+export * from './hotels-service';
+export * from './payments-service';
 export * from './tickets-service';
